1e9705ff070e36b2a4b96b33ea12e0eeef331181
bindothorpe <91731690+bindothorpe@users.noreply.github.com>
2024-03-24 22:17:10 -0500

    Refactor Hero component to use Next.js Image component

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,11 +1,17 @@
+import Image from "next/image";
 import MenuButton from "./MenuButton";
 import Title from "./Title";
 
 export default function Hero() {
   return (
-    <div
-      className={`relative w-full h-screen bg-cover bg-center bg-[url('/images/hero_black_and_white.jpg')]`}
-    >
+    <div className="relative w-full h-screen">
+      <Image
+        src="/images/hero_black_and_white.jpg"
+        alt="Hero Image"
+        layout="fill"
+        objectFit="cover"
+        quality={100}
+      />
       <div className="absolute top-0 right-0 p-4 pr-5">
         <MenuButton />
       </div>
