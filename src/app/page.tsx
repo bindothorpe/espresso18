eb3ff38730abf5b69ad9f2702b279b80f3d832d6
bindothorpe <91731690+bindothorpe@users.noreply.github.com>
2024-04-04 15:53:54 -0500

    Update layout and metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,6 @@ import Coffee from "./components/Coffee";
 import Menu from "./components/Menu";
 import Footer from "./components/Footer";
 import GeneralInformation from "./components/GeneralInformation";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Espresso18",
-};
 
 export default function Home() {
   return (
