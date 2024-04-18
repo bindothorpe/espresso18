2cd59c7e23a2c4dd2d2cb2f24db9ad55b61955b4
bindothorpe <91731690+bindothorpe@users.noreply.github.com>
2024-04-18 17:17:24 -0500

    Added empty image container

--- src/app/edit/components/images/ImageDisplayComponent.tsx
+++ src/app/edit/components/images/ImageDisplayComponent.tsx
@@ -1,8 +1,12 @@
 "use client";
-
 import Image from "next/image";
 import EditImageModal from "./EditImageModal";
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowUpFromBracket,
+  faImage,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function ImageDisplayComponent(props: {
   key: number;
@@ -19,13 +23,24 @@ export default function ImageDisplayComponent(props: {
       className="aspect-square relative group hover:cursor-pointer"
       onClick={() => setIsOpen(true)}
     >
-      <Image
-        fill
-        src={props.url}
-        alt={props.altText}
-        objectFit="cover"
-        className="rounded-lg"
-      />
+      {props.url ? (
+        <Image
+          fill
+          src={props.url}
+          alt={props.altText}
+          objectFit="cover"
+          className="rounded-lg"
+        />
+      ) : (
+        <div className="aspect-square bg-transparent rounded-lg border-2 border-dashed border-gray-400 flex justify-center items-center flex-col gap-4 group-hover:opacity-0 transition duration-300">
+          <FontAwesomeIcon
+            icon={faArrowUpFromBracket}
+            size="2x"
+            color="#222222"
+          />
+          <span>Upload an image</span>
+        </div>
+      )}
       <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-60 transition duration-300 rounded-lg"></div>
       <div className="absolute inset-0 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
         <div className="text-white text-base font-bold">{props.title}</div>
