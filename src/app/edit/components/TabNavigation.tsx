c316175478fddc4176f7cfd1ad6b2c9cc64f2b53
bindothorpe <91731690+bindothorpe@users.noreply.github.com>
2024-04-18 16:59:13 -0500

    Refactored some logic of tab navigation

--- src/app/edit/components/TabNavigation.tsx
+++ src/app/edit/components/TabNavigation.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { Tabs, Tab } from "@nextui-org/react";
 
 export default function TabNavigation(props: {
@@ -7,20 +6,26 @@ export default function TabNavigation(props: {
   locationAndHoursChildren: React.ReactNode;
   imagesChildren: React.ReactNode;
 }) {
+  const styling = {
+    tabList: "flex justify-between w-full",
+    tab: "flex-1 text-sm",
+  };
+
   return (
     <div>
-      <Tabs variant="underlined" color="primary" aria-label="Edit navigation">
-        <Tab key="menu" title="Menu" className="text-sm">
+      <Tabs
+        variant="underlined"
+        color="primary"
+        aria-label="Edit navigation"
+        className="w-[100%]"
+      >
+        <Tab key="menu" title="Menu">
           <div className="mt-20">{props.menuChildren}</div>
         </Tab>
-        <Tab
-          key="location-and-hours"
-          title="Location and Hours"
-          className="text-sm"
-        >
+        <Tab key="location-and-hours" title="Location and Hours">
           <div className="mt-20 mx-2">{props.locationAndHoursChildren}</div>
         </Tab>
-        <Tab key="images" title="Images" className="text-sm">
+        <Tab key="images" title="Images">
           <div className="mt-20 mx-2">{props.imagesChildren}</div>
         </Tab>
       </Tabs>
