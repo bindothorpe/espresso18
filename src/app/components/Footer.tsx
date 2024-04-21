29c30926ca8f6ca8eb08ef4936513ece7be118cd
bindothorpe <91731690+bindothorpe@users.noreply.github.com>
2024-04-21 14:38:35 -0500

    Updated footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,49 +1,51 @@
+"use client";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import Logo from "./Logo";
 
 export default function Footer() {
+  const navigateExternalTab = (url: string) => {
+    window.open(url, "_blank");
+  };
   return (
     <footer className="flex flex-col p-12 md:p-20 bg-white text-black">
+      {/* //Horizontal line */}
+      <div className="h-[2px] bg-black mb-8 md:mb-10"></div>
       {/* //Logo, links, socials */}
-      <div className="flex flex-col md:flex-row justify-between mb-8 md:mb-16 items-center gap-4">
+      <div className="flex flex-col md:flex-row justify-between items-center gap-4">
         {/* Logo */}
         <div className="flex justify-start md:w-1/4">
           <Logo />
         </div>
-        {/* Links */}
-        <div className="flex flex-row md:w-1/2 justify-center w-[100%]">
-          <div className="flex md:flex-row flex-wrap gap-x-5 justify-center">
-            <div className="text-sm">Link One</div>
-            <div className="text-sm">Link Two</div>
-            <div className="text-sm">Link Three</div>
-            <div className="text-sm">Link Four</div>
-            <div className="text-sm">Link Five</div>
-          </div>
-        </div>
-        {/* Socials */}
-        <div className="flex flex-row md:w-1/4 justify-end gap-x-5">
-          <div>
-            <FontAwesomeIcon icon={faInstagram} size="2x" />
-          </div>
-          <div>
-            <FontAwesomeIcon icon={faFacebookSquare} size="2x" />
-          </div>
-        </div>
-      </div>
-      {/* //Horizontal line */}
-      <div className="h-[2px] bg-black mb-8 md:mb-10"></div>
-      {/* //Copyright */}
-      <div className="flex flex-wrap md:flex-row gap-x-5 justify-center">
-        <div className="text-center">
+        <div className="hidden md:block text-center">
           <span className="text-sm">
             &copy; 2024 Espresso18. All rights reserved.
           </span>
         </div>
-        <div className="underline text-sm">Privacy Policy</div>
-        <div className="underline text-sm">Terms of Service</div>
-        <div className="underline text-sm">Cookie Settings</div>
+        <div className="flex flex-row md:w-1/4 justify-end gap-x-5">
+          <button
+            onClick={() =>
+              navigateExternalTab("https://www.instagram.com/espresso18cp/")
+            }
+          >
+            <FontAwesomeIcon icon={faInstagram} size="2x" />
+          </button>
+          <button
+            onClick={() =>
+              navigateExternalTab(
+                "https://www.facebook.com/espresso18cp?mibextid=LQQJ4d"
+              )
+            }
+          >
+            <FontAwesomeIcon icon={faFacebookSquare} size="2x" />
+          </button>
+        </div>
+        <div className="md:hidden text-center">
+          <span className="text-sm">
+            &copy; 2024 Espresso18. All rights reserved.
+          </span>
+        </div>
       </div>
     </footer>
   );
